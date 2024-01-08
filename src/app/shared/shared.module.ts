f4568e771d8ee72158db2cec7c4a1a3a33909bed
Dhirendra Singh Negi <dhirunegi07@gmail.com>
2024-01-08 22:02:55 +0530

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [HomeComponent,
+    NavbarComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomeComponent,
+    NavbarComponent]
+})
+export class SharedModule { }
